abd4d015b888b145997447ec9db1b9fead6bb8c6
Andrew D'Aoust <daoust.andrew@gmail.com>
2022-10-17 21:56:29 -0400

    Adding some custom react components

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,37 +1,70 @@
-// import React from 'react';
+import React from "react";
 
-// // export const Terminal = ({node, lines}) => {
-// export default Terminal = (lines) => {
-//   let prefix = 'test: $';
-//   // return (
-//   //   <div>
-//   //     <div>
-//   //       <span style="background:#f25f58"></span>
-//   //       <span style="background:#fbbe3c"></span>
-//   //       <span style="background:#58cb42"></span>
-//   //       <span>bash</span>
-//   //     </div>
-//   //     <div>
-//   //       {list.each((el) => {
-//   //         return (
-//   //           <div>
-//   //             el
-//   //           </div>
-//   //         )
-//   //       })}
-//   //     </div>
-//   //   </div>
-//   // )
-//   // =================================================
-//   // let fmtLines = [];
-//   // lines.each((l) => {
-//   //   fmtLines.push(<div>{`${prefix} ${l}`}</div>)
-//   // })
-//   // return (
-//   //   <span>
-//   //     {...fmtLines}
-//   //   </span>
-//   // )
-//   // =================================================
-//   return <div>{lines}</div>
-// };
+// TODO: Need to modify structure of props so that the dir can be passed
+export function Terminal(props) {
+  return (
+    <div style={{ borderRadius: "10px" }}>
+      <HeaderBar />
+      <TerminalText lines={props.lines} />
+    </div>
+  );
+}
+
+function HeaderButton(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: props.buttonColor,
+        borderRadius: "100%",
+        width: "10px",
+        height: "10px",
+        margin: "3px",
+      }}
+    />
+  );
+}
+
+function HeaderBar() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "#edeeef",
+        padding: "6px",
+        borderTopLeftRadius: "10px",
+        borderTopRightRadius: "10px",
+      }}
+    >
+      <HeaderButton buttonColor="#FF605C" />
+      <HeaderButton buttonColor="#FFBD44" />
+      <HeaderButton buttonColor="#00CA4E" />
+    </div>
+  );
+}
+
+function CodeLine(props) {
+  let dir = props.dir ? props.dir : "~";
+
+  return (
+    <p style={{ margin: "0px", padding: "2px" }}>{`${dir}:$ ${props.text}`}</p>
+  );
+}
+
+function TerminalText(props) {
+  console.log(props.lines);
+  let lines = props.lines.map((line) => <CodeLine text={line.text} dir={line.dir} />);
+
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "green",
+        fontFamily: "monospace",
+        borderBottomLeftRadius: "10px",
+        borderBottomRightRadius: "10px",
+      }}
+    >
+      {lines}
+    </div>
+  );
+}
